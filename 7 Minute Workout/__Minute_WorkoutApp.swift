ee54aced26099ff3e80ac19db4b3f04a809553b8
Resn4R <purview-joust.0x@icloud.com>
2023-10-31 19:52:33 +0000

    Initial Commit

---
+++ 7 Minute Workout/__Minute_WorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Minute_WorkoutApp.swift
+//  7 Minute Workout
+//
+//  Created by Vito Borghi on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct __Minute_WorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
