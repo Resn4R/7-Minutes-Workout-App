2dac20e8797abc199979088c9802c9615f919994
Resn4R <purview-joust.0x@icloud.com>
2023-11-02 17:21:09 +0000

    added toggles to settingsView

---
+++ 7 Minute Workout/Views/AboutView.swift
@@ -0,0 +1,22 @@
+//
+//  AboutView.swift
+//  7 Minute Workout
+//
+//  Created by Vito Borghi on 02/11/2023.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        NavigationStack{
+            Text("")
+                .navigationTitle("About this app")
+                .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    AboutView()
+}
